4125be944cb8eca6bcc5a8d024d2813149d6f047
leidy pinzon <sanchezrodriguezheilyntatiana@gmail.com>
2026-08-20 00:31:05 -0500

    Actualizar mensaje de bienvenida

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def inicio():
-    return "<h1>¡Hola, Docker y Nginx!</h1><p>La aplicación está funcionando correctamente.</p>"
+    return "<h1>¡Hola, Docker y Nginx!</h1><p>Despliegue automático actualizado el 20 de agosto de 2026 🚀</p>"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5050, debug=True)
